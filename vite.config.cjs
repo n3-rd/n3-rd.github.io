d78288b166baeb2dcb332b96e1a2166da78ac1b0
n3-rd <godwinjdayo@gmail.com>
2022-08-12 02:31:56 +0100

    fixes

---
+++ vite.config.cjs
@@ -0,0 +1,17 @@
+// module.exports = {
+//     root: '/',
+//     build: {
+//       outDir: 'docs'
+//     }
+//   }
+  
+
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    root: '',
+    build: {
+        outDir: 'docs'
+    }
+})
