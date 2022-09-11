f8434cc990f97ce9145ca90386e89e55dd02c073
n3-rd <godwinjdayo@gmail.com>
2022-09-11 17:21:22 +0100

    added noise

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,11 +6,13 @@ import Preloader from '../components/Preloader.vue';
 import Header from '../components/Header.vue';
 import Footer from '../components/Footer.vue';
 import Cursor from '../components/Cursor.vue';
+import Noise from '../components/Noise.vue';
 
 </script>
 
 <template>
   <main class="bg-black">
+    <Noise class="fixed"/>
     <Preloader/>
     <Cursor/>
     <Header/>
