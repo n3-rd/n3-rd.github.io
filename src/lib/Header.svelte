6153dddec91ce40d561df64fa84eb2c0f4e57395
n3-rd <godwinjdayo@gmail.com>
2023-03-17 01:09:10 +0100

    added header

---
+++ src/lib/Header.svelte
@@ -0,0 +1,7 @@
+<div class="h-4 pt-7 px-10 w-screen flex justify-between items-center absolute text-white">
+	<div class="header-title">G.</div>
+	<div class="header-links flex justify-between items-center gap-7">
+		<div class="about">About</div>
+		<div class="github">Github</div>
+	</div>
+</div>
