ac483336b54579d8b4308051083882e670038651
n3-rd <godwinjdayo@gmail.com>
2023-03-17 01:46:01 +0100

    added gh-pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,16 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter()
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: '#svelte',
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: null
+    })
   },
   preprocess: vitePreprocess()
 };
