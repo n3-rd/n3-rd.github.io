f45042f0a562d48dce8f5e0caaa6dae37f907da3
n3-rd <godwinjdayo@gmail.com>
2023-03-17 15:50:09 +0100

    moved header to layout

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,7 +1,11 @@
+<script>
+	import { base } from '$app/paths';
+</script>
+
 <div class="h-4 pt-7 px-10 w-screen flex justify-between items-center absolute text-white">
-	<div class="header-title">G.</div>
+	<div class="header-title"><a href="/">G.</a></div>
 	<div class="header-links flex justify-between items-center gap-7">
-		<div class="about">About</div>
+		<div class="about"><a href="{base}/about">About</a></div>
 		<div class="github">Github</div>
 	</div>
 </div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script>
 	import '../app.scss';
+	import Header from '../lib/Header.svelte';
 	// src/routes/+layout.js
 	export const prerender = true;
 </script>
 
+<Header />
 <slot />
