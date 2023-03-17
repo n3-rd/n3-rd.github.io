d23352797cbaf8919788cc95ea90e558713a2ae7
n3-rd <godwinjdayo@gmail.com>
2023-03-17 20:19:34 +0100

    added noise

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,13 @@
 <script>
 	import Header from '../lib/Header.svelte';
+	import Noise from '../lib/Noise.svelte';
 	import Preloader from '../lib/Preloader.svelte';
 	import Swiper from '../lib/Swiper.svelte';
 	export const prerender = true;
 </script>
 
 <div class="bg-black select-none">
+	<Noise />
 	<Preloader />
 	<Swiper />
 </div>
