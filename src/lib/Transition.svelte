dfe610d386a126c628fd867e105b609e044b3155
n3-rd <godwinjdayo@gmail.com>
2023-03-17 17:33:30 +0100

    added page transition

---
+++ src/lib/Transition.svelte
@@ -0,0 +1,8 @@
+<!-- src/component/PageTransitions.svelte -->
+<script>
+	import { fly } from 'svelte/transition';
+</script>
+
+<div in:fly={{ y: -50, duration: 250, delay: 300 }} out:fly={{ y: -50, duration: 250 }}>
+	<slot />
+</div>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,1 +1,14 @@
-<h1>About</h1>
+<!-- src/routes/about.svelte -->
+<script>
+	// import PageTransitions from '/PageTransitions.svelte';
+
+	import Transition from '../../lib/Transition.svelte';
+</script>
+
+<Transition>
+	<main>
+		<h1>About Page</h1>
+		<p>This is the about page</p>
+		<p>More paragraphs of the about page</p>
+	</main>
+</Transition>
