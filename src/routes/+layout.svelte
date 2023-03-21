26a4e0a8c6e2f6b95d150f0feefeb09ad9afdc34
n3-rd <godwinjdayo@gmail.com>
2023-03-21 22:46:38 +0100

    moved preloader to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,12 @@
 	import '../app.scss';
 	import Header from '../lib/Header.svelte';
 	import Noise from '../lib/Noise.svelte';
+	import Preloader from '../lib/Preloader.svelte';
 	// src/routes/+layout.js
 	export const prerender = true;
 </script>
 
 <Noise />
+<Preloader />
 <Header />
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,12 @@
 <script>
 	// import Noise from '../lib/Noise.svelte';
-	import Preloader from '../lib/Preloader.svelte';
+	// import Preloader from '../lib/Preloader.svelte';
 	import Swiper from '../lib/Swiper.svelte';
 	export const prerender = true;
 </script>
 
 <div class="bg-black select-none">
-	<Preloader />
+	<!-- <Preloader /> -->
 	<Swiper />
 	<div class="absolute bottom-3 left-3 text-lg z-50 capitalize">
 		<span class="hidden md:inline-block">Drag or </span> swipe
