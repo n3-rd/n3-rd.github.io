dc89deb1816563d8319a8529aa44d0d919036267
n3-rd <godwinjdayo@gmail.com>
2023-03-21 22:48:06 +0100

    added route transition to root page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,14 +2,17 @@
 	// import Noise from '../lib/Noise.svelte';
 	// import Preloader from '../lib/Preloader.svelte';
 	import Swiper from '../lib/Swiper.svelte';
+	import Transition from '../lib/Transition.svelte';
 	export const prerender = true;
 </script>
 
-<div class="bg-black select-none">
-	<!-- <Preloader /> -->
-	<Swiper />
-	<div class="absolute bottom-3 left-3 text-lg z-50 capitalize">
-		<span class="hidden md:inline-block">Drag or </span> swipe
+<Transition>
+	<div class="bg-black select-none">
+		<!-- <Preloader /> -->
+		<Swiper />
+		<div class="absolute bottom-3 left-3 text-lg z-50 capitalize">
+			<span class="hidden md:inline-block">Drag or </span> swipe
+		</div>
+		<div class="absolute bottom-3 right-3 text-lg z-50 capitalize">Double click to visit url</div>
 	</div>
-	<div class="absolute bottom-3 right-3 text-lg z-50 capitalize">Double click to visit url</div>
-</div>
+</Transition>
