f380f4beba724237f3b488123558d48ef61dd46e
n3-rd <godwinjdayo@gmail.com>
2023-03-21 21:59:56 +0100

    moved noise component to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script>
 	import '../app.scss';
 	import Header from '../lib/Header.svelte';
+	import Noise from '../lib/Noise.svelte';
 	// src/routes/+layout.js
 	export const prerender = true;
 </script>
 
+<Noise />
 <Header />
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,11 @@
 <script>
-	import Noise from '../lib/Noise.svelte';
+	// import Noise from '../lib/Noise.svelte';
 	import Preloader from '../lib/Preloader.svelte';
 	import Swiper from '../lib/Swiper.svelte';
 	export const prerender = true;
 </script>
 
 <div class="bg-black select-none">
-	<Noise />
 	<Preloader />
 	<Swiper />
 	<div class="absolute bottom-3 left-3 text-lg z-50 capitalize">
