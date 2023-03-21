ac3b17cba5d38bdcdce4e31f85bf5f93764071e7
n3-rd <godwinjdayo@gmail.com>
2023-03-21 22:59:22 +0100

    added contact page

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -6,7 +6,7 @@
 	<div class="header-title"><a href="/">G.</a></div>
 	<div class="header-links hidden md:flex justify-between items-center gap-7">
 		<div class="about"><a href="{base}/about">About</a></div>
-		<div class="github">Github</div>
+		<div class="contact"><a href="{base}/contact">Contact</a></div>
 	</div>
 	<div class="menu md:hidden">menu</div>
 </div>
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,49 @@
+<script>
+	import Transition from '../../lib/Transition.svelte';
+	import { onMount } from 'svelte';
+	const playSound = () => {
+		var audio = new Audio('/sound/click.mp3');
+		audio.volume = 0.3;
+		audio.play();
+	};
+
+	onMount(() => {
+		const contactLink = document.querySelectorAll('.link');
+		contactLink.forEach((e) => {
+			e.addEventListener('mouseenter', () => {
+				playSound();
+			});
+		});
+	});
+</script>
+
+<Transition>
+	<div class="h-screen w-screen flex justify-center items-center">
+		<div class="contact-links morgenwalsh text-[5rem] text-[#b3b3b3]">
+			<a
+				href="/"
+				target="_blank"
+				rel="noreferrer"
+				class="block link cursor-pointer hover:text-white">Email</a
+			>
+			<a
+				href="https://github.com/n3-rd"
+				target="_blank"
+				rel="noreferrer"
+				class="block link cursor-pointer hover:text-white">Github</a
+			>
+			<a
+				href="/"
+				target="_blank"
+				rel="noreferrer"
+				class="block link cursor-pointer hover:text-white">LinkedIn</a
+			>
+			<a
+				href="https://twitter.com/web_n3rd"
+				target="_blank"
+				rel="noreferrer"
+				class="block link cursor-pointer hover:text-white">Twitter</a
+			>
+		</div>
+	</div>
+</Transition>
