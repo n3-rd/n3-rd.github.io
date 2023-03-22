ea637a73542d3c26b52b3ae4910dc16c8cc66b31
n3-rd <godwinjdayo@gmail.com>
2023-03-22 22:13:55 +0100

    mobile header reverted to previous version

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -4,9 +4,8 @@
 
 <div class="h-4 pt-7 px-10 w-screen flex justify-between items-center fixed text-white">
 	<div class="header-title"><a href="/">G.</a></div>
-	<div class="header-links hidden md:flex justify-between items-center gap-7">
+	<div class="header-links flex justify-between items-center gap-7">
 		<div class="about"><a href="{base}/about">About</a></div>
 		<div class="contact"><a href="{base}/contact">Contact</a></div>
 	</div>
-	<div class="menu md:hidden">menu</div>
 </div>
