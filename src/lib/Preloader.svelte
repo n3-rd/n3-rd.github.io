0f26bc0b742879ae6579fb55bf2a96ba5f6c4b0d
n3rd <godwinjdayo@gmail.com>
2023-10-11 11:43:42 +0100

    added preloader

---
+++ src/lib/Preloader.svelte
@@ -0,0 +1,22 @@
+<script>
+
+/**
+ * type: {number}
+ */
+let count = 0;
+let interval = setInterval(() => {
+    count++;
+   
+    if (count === 100) {
+        clearInterval(interval);
+
+    }
+}, 20);
+  
+</script>
+<div class="preloader w-screen h-screen bg-white flex items-center">
+    <div class="preloader-mask h-full z-[30] bg-white fixed mix-blend-difference"
+        style="width: {count}%;"
+    ></div>
+    <div class="preloader-counter text-black text-[22vw]">{count}%</div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script>
     import "../app.css";
   </script>
-  
+  <svelte:head>
+    <link href="https://api.fontshare.com/v2/css?f[]=panchang@1&display=swap" rel="stylesheet">
+  </svelte:head>
   <slot />
