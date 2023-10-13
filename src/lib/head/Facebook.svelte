62bb896244cd8a398d3eb51600a9d52713c7dc6c
n3rd <godwinjdayo@gmail.com>
2023-10-13 05:47:14 +0100

    👌 IMPROVE: Opengraph links

--- src/lib/head/Facebook.svelte
+++ src/lib/head/Facebook.svelte
@@ -1,10 +1,10 @@
-<!-- Open Graph / Facebook -->
+<!-- Facebook Meta Tags -->
+<meta property="og:url" content="https://n3rd.vercel.app" />
 <meta property="og:type" content="website" />
-<meta property="og:url" content="https://n3-rd.github.io" />
 <meta property="og:title" content="Godwin Jemegah - Web Developer" />
 <meta
 	property="og:description"
-	content="I am a web developer and designer based in the city of Lagos, Nigeria I create websites and web apps that are responsive, fast and accessible. with the latest and trusted web technology known to man :)."
+	content="I am a web developer and designer based in the city of Lagos, Nigeria I create websites and fast and accessible."
 />
 <meta
 	property="og:image"
--- src/lib/head/Twitter.svelte
+++ src/lib/head/Twitter.svelte
@@ -1,12 +1,15 @@
-<!-- Twitter -->
-<meta property="twitter:card" content="summary_large_image" />
-<meta property="twitter:url" content="https://n3-rd.github.io" />
-<meta property="twitter:title" content="Godwin Jemegah - Web Developer" />
+<!-- Twitter Meta Tags -->
+<meta name="twitter:card" content="summary_large_image" />
+<meta property="twitter:domain" content="n3rd.vercel.app" />
+<meta property="twitter:url" content="https://n3rd.vercel.app" />
+<meta name="twitter:title" content="Godwin Jemegah - Web Developer" />
 <meta
-	property="twitter:description"
-	content="I am a web developer and designer based in the city of Lagos, Nigeria I create websites and web apps that are responsive, fast and accessible. with the latest and trusted web technology known to man :)."
+	name="twitter:description"
+	content="I am a web developer and designer based in the city of Lagos, Nigeria I create websites and fast and accessible."
 />
 <meta
-	property="twitter:image"
+	name="twitter:image"
 	content="https://www.linkpicture.com/q/Screenshot-from-2023-10-13-05-25-33.png"
 />
+
+<!-- Meta Tags Generated via https://www.opengraph.xyz -->
